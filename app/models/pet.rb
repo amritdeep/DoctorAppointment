675e94c18dae3237f987a39475572ced9b631f6b
AmritDeep <amritdeepdhungana@hotmail.com>
2016-01-05 01:30:11 -0600

    validates pet model along with its test

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,9 @@
 class Pet < ActiveRecord::Base
+	validates :name, presence: true, length: { maximum: 32 }
+	validates :types, presence: true, inclusion: { in: %w(dog cat bird), 
+	message: "%{value} isn't included" }
+	validates :breed, presence: true, length: { maximum: 32 }
+	validates :age, presence: true, numericality: { only_integer: true }
+	validates :weight, presence: true, numericality: { only_integer: true }
+	validates :date_of_last_vist, presence: true
 end
--- test/models/pet_test.rb
+++ test/models/pet_test.rb
@@ -4,4 +4,55 @@ class PetTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "Name of Pet - required, no more than 35 characters" do
+  	p = Pet.new
+  	p.name = "JohnJohnJohnJohnJohnJohnJohnJohnJohn"
+  	assert_not p.save
+  end
+
+  test "Type of Pet - must be a dog, cat, or bird" do
+  	p = Pet.new
+  	p.name = "John"
+  	p.types = "car"
+  	assert_not p.save
+  end
+
+  test "Breed - required, no more than 35 characters" do
+  	p = Pet.new
+  	p.name = "John"
+  	p.types = "car"
+  	p.breed = "LongLongLongLongLongLongLongLongLongLong"
+  	assert_not p.save
+  end
+
+  test "Age – required" do
+  	p = Pet.new
+  	p.name = "John"
+  	p.types = "dog"
+  	p.breed = "Long"
+  	p.age = "abc"
+  	assert_not p.save
+  end
+
+  test "Weight - required" do
+  	p = Pet.new
+  	p.name = "John"
+  	p.types = "dog"
+  	p.breed = "Long"
+  	p.age = 23
+  	p.weight = 'axf'
+  	assert_not p.save
+  end
+
+  test "Date of Last Visit - required" do
+  	p = Pet.new
+  	p.name = "John"
+  	p.types = "dog"
+  	p.breed = "Long"
+  	p.age = 23
+  	p.weight = 'axf'
+  	assert_not p.save
+  end
+
 end
